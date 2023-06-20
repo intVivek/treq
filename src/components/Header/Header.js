a030cdfeb8b8a373d498f4a0abeb0847c9e9f467
intVivek <viveksrivastava6180@gmail.com>
2023-06-20 20:18:45 +0530

    Fixed Header and added functionality in toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.scss";
 import Avatar from "../Avatar/Avatar";
 import Toggle from "../Toggle/Toggle";
 
-const toggleListItems = [
-  "Dashboard",
-  "Accounts",
-  "Payments",
-  "Post-Export Documents",
-];
+const toggleListItems = {
+  DashBoard: 0,
+  Accounts: 1,
+  Payments: 2,
+  "Post-Export Documents": 3,
+};
 
 const Header = () => {
+  const [activeToggle, setActiveToggle] = useState(toggleListItems.DashBoard);
+
   return (
     <div className="treq-header">
       <div className="topSection">
         <label>Welcome back Mr. Rahul</label>
         <Avatar />
       </div>
-      <Toggle list={toggleListItems} />
+      <Toggle
+        active={activeToggle}
+        onClick={setActiveToggle}
+        list={Object.keys(toggleListItems)}
+      />
     </div>
   );
 };
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Toggle.scss";
 
-const Toggle = ({ list, active }) => {
+const Toggle = ({ list, active, onClick }) => {
   return (
     <div className={"treq-toggle"}>
       {list.map((item, index) => (
-        <div className={0 === index && "active"} key={index}>
+        <div
+          onClick={() => onClick(index)}
+          className={active === index && "active"}
+          key={index}
+        >
           {item}
         </div>
       ))}
